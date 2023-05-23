a88f0a8868012317989eeffa99ac60d60e8fe411
Marta <sliusar18@gmail.com>
2023-05-23 14:24:08 +0300

    correct

--- task1.py
+++ task1.py
@@ -1,8 +1,6 @@
-from collections import Counter
 def find_unique_value(numbers):
     for el in numbers:
         if numbers.count(el) == 1:
             return el
 
-numbers = [2, 3, 3, 3]
-print(find_unique_value(numbers))
+print(find_unique_value(input("Введіть числа:")))
